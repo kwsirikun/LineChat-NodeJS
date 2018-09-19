25d1335c8725dd85b946c8ea56ad058cab14c57b
kwsirikun <43022900+kwsirikun@users.noreply.github.com>
2018-09-19 14:10:44 +0700

    Update line-api-service.js

--- src/services/line-api-service.js
+++ src/services/line-api-service.js
@@ -1,5 +1,5 @@
 const request = require('request');
-const apiToken = 'ScXXvFL5OVLPWBs5wOVdf42WgFbNcMHeRGW/vzCgsQWtUUrQuACct48IWkinDmQLKBg7QuTuwpF++WdD4zwgxcKzZ9C5tUu7EdBhRPzPDpmXuveBoY4mhPxOtGrfXZryCaSR96g1Vg0aP+kwBbNJFwdB04t89/1O/w1cDnyilFU=';
+const apiToken = 'bK9JGXo5u4sFzy0dT0G/nHoCWCex4eevXxRxaP8YkvlJZiOUIYdlRqk2PBBV6U/9dZP3mQd1Vy7E38DpT0NcnkvV6Fdo3x0LytmLq0DvbExVJAS0PIm2PQGX2IuCRmd53jmcPUCqwE9J2Qi0Q1kyZgdB04t89/1O/w1cDnyilFU=';
 const apiRoute = 'https://api.line.me/v2/bot/message/reply';
 const headers = {
     'Content-Type': 'application/json',
