2b047f60419e7f9f01677f715e23ef02188aef65
kwsirikun <43022900+kwsirikun@users.noreply.github.com>
2018-11-19 08:42:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,16 @@ server()
             default:
                
 
-            lineMessaging.replyMessage(replyToken, 'Hello World').then(function (rs) {  
+            var request = require('request');
+                request.post({
+                url:     'http://10.211.70.35:18972/PAYGATEWAY_V2/MEAOPS_QUERY',
+                form:    { req: "MEACA"+ca+"MEATSTH" }
+                }, function(error, response, body){
+                console.log(body);
+
+
+
+            lineMessaging.replyMessage(replyToken, body).then(function (rs) {  
                 console.log(`Reply message result : ${ rs }`);    
                 res.json({
                     status: 200,
@@ -80,18 +89,11 @@ server()
 
 
 
-
-
-
         }
   
   
   
-  
-  
-  
-  
-        
+         
 
 
 
